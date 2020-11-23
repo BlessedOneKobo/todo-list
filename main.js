48370d8ea99d467d8a95030db77fe3467dc19138
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-11-24 00:50:41 +0100

    feat: Implement menu toggle

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 'use strict';
 
 const arrayForEach = Array.prototype.forEach;
+
+const menuElm = document.getElementById('menu');
 const deadAnchorList = document.querySelectorAll('a[href="#"]');
 const taskCheckboxList = document.getElementsByClassName('task-checkbox');
 const toggleBtnList = document.getElementsByClassName('task-card-toggle');
+
+const removeActiveClass = anchorList => {
+  arrayForEach.call(anchorList, anchorElm => {
+    anchorElm.classList.remove('active');
+  });
+};
+
+if (menuElm) {
+  const menuAnchorList = menuElm.getElementsByTagName('a');
+  arrayForEach.call(menuAnchorList, anchorElm => {
+    anchorElm.addEventListener('click', event => {
+      event.preventDefault();
+
+      removeActiveClass(menuAnchorList);
+      anchorElm.classList.add('active');
+    });
+  });
+}
 
 arrayForEach.call(deadAnchorList, anchorElm => {
   anchorElm.addEventListener('click', ev => {
