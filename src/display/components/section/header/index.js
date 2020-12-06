efde28683e4fc1da8f648484377b433df15d1b83
BlessedOneKobo <BlessedOneKobo@protonmail.com>
2020-12-06 20:29:45 +0100

    fix: Create folder for section-header

---
+++ src/display/components/section/header/index.js
@@ -0,0 +1,25 @@
+import { on } from '../../../../events';
+
+import './style.css';
+
+const sectionHeader = document.createElement('div');
+sectionHeader.className = 'section-header projects';
+
+const deleteBtn = document.createElement('button');
+deleteBtn.className = 'btn delete';
+deleteBtn.textContent = 'Delete Completed Tasks';
+
+const createBtn = document.createElement('button');
+createBtn.className = 'btn create';
+createBtn.textContent = 'Create Project';
+
+sectionHeader.appendChild(deleteBtn);
+sectionHeader.appendChild(createBtn);
+
+on('toggleNavigation', ({ selected }) => {
+  sectionHeader.className = `section-header ${selected}`;
+
+  createBtn.textContent = selected === 'projects' ? 'Create Project' : 'Create Task';
+});
+
+export default sectionHeader;
--- src/display/components/section/index.js
+++ src/display/components/section/index.js
@@ -1,29 +1,8 @@
-import { on } from '../../../events';
-
-import './style.css';
+import sectionHeader from './header';
 
 const section = document.createElement('div');
 section.className = 'section';
 
-const sectionHeader = document.createElement('div');
-sectionHeader.className = 'section-header projects';
-
-const deleteBtn = document.createElement('button');
-deleteBtn.className = 'btn delete';
-deleteBtn.textContent = 'Delete Completed Tasks';
-
-const createBtn = document.createElement('button');
-createBtn.className = 'btn create';
-createBtn.textContent = 'Create Project';
-
-sectionHeader.appendChild(deleteBtn);
-sectionHeader.appendChild(createBtn);
 section.appendChild(sectionHeader);
-
-on('toggleNavigation', ({ selected }) => {
-  sectionHeader.className = `section-header ${selected}`;
-
-  createBtn.textContent = selected === 'projects' ? 'Create Project' : 'Create Task';
-});
 
 export default section;
